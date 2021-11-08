770cb118d611d24ce3f8c4158af3e64251b02066
Seymur Abbasov <seymur.abbasov.q@student.aztu.edu.az>
2021-11-08 13:42:10 +0400

    Add project files.

---
+++ C-Task-CR-1.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace C_Task_CR_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*  int[] Arry = { };*/
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            #region Task-1
+            int[] Arry = new int[0];
+
+            for (var i = 0; i < 5; i++)
+            {
+                /*if (Arry == null)
+                    Arry = new int[1];*/
+
+                /*  else*/
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Array.Resize(ref Arry, Arry.Length + 1);
+
+                Arry[Arry.Length - 1] = a;
+
+                Console.WriteLine("=-----------------------");
+
+
+            }
+            for (int i = 0; i < Arry.Length; i++)
+            {
+                Console.WriteLine(Arry[i]);
+            }
+
+            #endregion
+
+            #region Task-2
+                
+            #endregion
+
+        }
+
+    }
+}
